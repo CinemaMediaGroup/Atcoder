bd810b3751628414cf4858c8cdb9e9cfe47d099b
louisjshen <i@pixiv.world>
2020-07-14 21:45:48 +0800

    AtCoder Beginner Contest 001 C (finished)

---
+++ Atcoder/ABC001/C/C.swift
@@ -0,0 +1,89 @@
+//
+//  C.swift
+//  Atcoder
+//
+//  Created by Louis Joe Shen on 2020/7/14.
+//  Copyright © 2020 Louise Joe Shen. All rights reserved.
+//
+
+import Foundation
+
+/*
+var input = readLine();
+var inputs : [String] = input!.components(separatedBy: " ");
+
+var deg : Double = Double(inputs[0])!;
+var dis : Double = Double(inputs[1])!;
+var speed : Double = Double(lround((dis * 10 / 60.0))) / 10.0;
+
+var dir : String = "N";
+var W : Int = 0;
+
+if(112.5 <= deg && deg < 337.5){
+    dir = "NNE";
+}else if(337.5 <= deg && deg < 562.5){
+    dir = "NE";
+}else if(562.5 <= deg && deg < 787.5){
+    dir = "ENE";
+}else if(787.5 <= deg && deg < 1012.5){
+    dir = "E";
+}else if(1012.5 <= deg && deg < 1237.5){
+    dir = "ESE";
+}else if(1237.5 <= deg && deg < 1462.5){
+    dir = "SE";
+}else if(1462.5 <= deg && deg < 1687.5){
+    dir = "SSE";
+}else if(1687.5 <= deg && deg < 1912.5){
+    dir = "S";
+}else if(1912.5 <= deg && deg < 2137.5){
+    dir = "SSW";
+}else if(2137.5 <= deg && deg < 2362.5){
+    dir = "SW";
+}else if(2362.5 <= deg && deg < 2587.5){
+    dir = "WSW";
+}else if(2587.5 <= deg && deg < 2812.5){
+    dir = "W";
+}else if(2812.5 <= deg && deg < 3037.5){
+    dir = "WNW";
+}else if(3037.5 <= deg && deg < 3262.5){
+    dir = "NW";
+}else if(3262.5 <= deg && deg < 3487.5){
+    dir = "NNW";
+}else{
+    dir = "N";
+}
+
+if(0 <= speed && speed <= 0.2){
+    W = 0;
+}else if(0.3 <= speed && speed <= 1.5){
+    W = 1;
+}else if(1.6 <= speed && speed <= 3.3){
+    W = 2;
+}else if(3.4 <= speed && speed <= 5.4){
+    W = 3;
+}else if(5.5 <= speed && speed <= 7.9){
+    W = 4;
+}else if(8.0 <= speed && speed <= 10.7){
+    W = 5;
+}else if(10.8 <= speed && speed <= 13.8){
+    W = 6;
+}else if(13.9 <= speed && speed <= 17.1){
+    W = 7;
+}else if(17.2 <= speed && speed <= 20.7){
+    W = 8;
+}else if(20.8 <= speed && speed <= 24.4){
+    W = 9;
+}else if(24.5 <= speed && speed <= 28.4){
+    W = 10;
+}else if(28.5 <= speed && speed <= 32.6){
+    W = 11;
+}else if(32.7 <= speed){
+    W = 12;
+}
+
+if(W == 0){
+    dir = "C";
+}
+
+print(dir,W);
+*/
