9b41614b726df91c41fe1a7774742ea096e2b8e6
louisjshen <i@pixiv.world>
2020-07-14 13:01:50 +0800

    Initial Commit

---
+++ Atcoder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Atcoder
+//
+//  Created by Louis Joe Shen on 2020/7/14.
+//  Copyright © 2020 Louise Joe Shen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
