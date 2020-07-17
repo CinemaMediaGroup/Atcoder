3beb6b760cc9756f2e1a4ac70f44c494e310e628
louisjshen <i@pixiv.world>
2020-07-17 13:55:31 +0800

    ABC042 A finished

---
+++ Atcoder/ABC042/A042.swift
@@ -0,0 +1,23 @@
+//
+//  A.swift
+//  Atcoder
+//
+//  Created by Louis Joe Shen on 2020/7/17.
+//  Copyright © 2020 Louise Joe Shen. All rights reserved.
+//
+
+import Foundation
+/*
+var input = readLine()
+var inputs : [String] = input!.components(separatedBy: " ")
+
+var a = Int(inputs[0])!
+var b = Int(inputs[1])!
+var c = Int(inputs[2])!
+
+if(a + b + c == 17){
+    print("YES")
+}else{
+    print("NO")
+}
+*/
--- Atcoder/main.swift
+++ Atcoder/main.swift
@@ -2,8 +2,9 @@
 //  main.swift
 //  Atcoder
 //
-//  Created by Louis Joe Shen on 2020/7/14.
+//  Created by Louis Joe Shen on 2020/7/17.
 //  Copyright © 2020 Louise Joe Shen. All rights reserved.
 //
 
-import Foundation;
+import Foundation
+
